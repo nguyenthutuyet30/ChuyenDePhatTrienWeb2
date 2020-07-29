150444850c2841f2d5646db3d8b1cbb5d76669b9
thutuyet30 <nthutuyet98@gmail.com>
2020-07-29 20:12:33 +0700

    update modul 29
    Update lại modul 29 - sửa phần carousel hình ảnh

---
+++ Modul Project - CDW2/29/29-content.php
@@ -0,0 +1,175 @@
+<?php
+    $url_host = $_SERVER['HTTP_HOST'];
+
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+    preg_match_all($pattern_uri, __DIR__, $matches);
+
+    $url_path = $url_host . $matches[1][0];
+
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+
+<div class="type-29">
+    <div class="container">
+        <!-- Thiếu menu nhớ bổ sung -->
+        <div class="detail_description">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="images/popular-properties-1" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="images/popular-properties-2" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="images/popular-properties-3" alt="Third slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <ul class="list-img d-flex flex-row">
+                        <li><img src="images/popular-properties-1" alt="" class="w-100"></li>
+                        <li><img src="images/popular-properties-2" alt="" class="w-100"></li>
+                        <li><img src="images/popular-properties-3" alt="" class="w-100"></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <h3>For Sale</h3>
+                    <ul class="table_info">
+                        <li><b>Created on:</b>&emsp;09-11-2011 00:00:00</li>
+                        <li><b>Square Free:</b>&emsp;80 Sqmt</li>
+                        <li><b>Bedroom:</b>&emsp;4</li>
+                        <li><b>Bathrooms:</b>&emsp;2</li>
+                        <li><b>Rooms:</b>&emsp;5</li>
+                        <li><b>Floors:</b>&emsp;1</li>
+                        <li><b>Rating:</b>&emsp;<i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"
+                                aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i
+                                class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>
+                        </li>
+                        <li><b>Category:</b>&emsp;Villa</li>
+                        <li><b>Tags:</b>&emsp;#Ciputra #A1</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="report">
+            <div class="property-title">
+                <h2>CIPUTRA A1 <a href=""><img src="images/report.png" alt=""></a></h2>
+                <span class="pull-right">€ 500.00</span>
+                <span>180 West College Street, Abbeville, AL</span>
+            </div>
+
+            <div class="content_text">
+                <div class="row">
+                    <div class="col-lg-9 col-12">
+                        <div class="text-justify">
+                            <p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.
+                            </p>
+                            <p>littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas
+                                humanitatis
+                                per
+                                seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum
+                                clari, fiant
+                                sollemnes in futurum.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-12">
+                        <div class="box-note">
+                            <strong>Open for Inspection Times</strong>
+                            <div class="note_small">No inspections are currently scheduled</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="property_meta">
+            <span><i class="fa fa-arrows" aria-hidden="true"></i>&nbsp;Get direction</span>
+            <span class="pull-right">
+                <i class="fa fa-print" aria-hidden="true"></i><span>&nbsp;Print this page</span>&emsp;
+                <i class="fa fa-file-pdf-o" aria-hidden="true"></i><span>&nbsp;PDF</span>&emsp;
+                <i class="fa fa-th-list" aria-hidden="true"></i><span>&nbsp;Add to compare list</span>
+            </span>
+        </div>
+
+        <div class="infomation_detail mt-4 mb-4">
+            <h4>PROPERTY INFORMATION</h4>
+            <div class="row">
+                <ul class="col-lg-6 col-12">
+                    <li><span class="text-boild">Fact</span></li>
+                    <li><span class="text-boild">Lot:</span>&emsp;125.00</li>
+                    <li><span class="text-boild">Built In:</span>&emsp;2005</li>
+                    <li><span class="text-boild">Contruction</span></li>
+                    <li><span class="text-boild">Unit Count:</span>&emsp;1</li>
+                    <li><span class="text-boild">Laudry:</span>&emsp;In Unit</li>
+                </ul>
+                <ul class="col-lg-6 col-12 col-12">
+                    <li><span class="text-boild">Cooling:</span>&emsp;Center</li>
+                    <li><span class="text-boild">Heating:</span>&emsp;Center</li>
+                    <li><span class="text-boild">Single Family:</span>&emsp;Yes</li>
+                    <li><span class="text-boild">Structure Type:</span>&emsp;Other</li>
+                    <li><span class="text-boild">Other info</span></li>
+                    <li><span class="text-boild">Pare#:</span>&emsp;10000000121212000</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="infomation_detail">
+            <h4>NEIGHBORHOOD</h4>
+            <div class="row">
+                <ul class="col-lg-6 col-12">
+                    <li><span class="text-boild">School:</span>&emsp;25 minutes by Car</li>
+                    <li><span class="text-boild">Coffee Shop:</span>&emsp;5 minutes by Walk</li>
+                </ul>
+                <ul class="col-lg-6 col-12 col-12">
+                    <li><span class="text-boild">Cinima:</span>&emsp;15 minutes by Walk</li>
+                    <li><span class="text-boild">Airport:</span>&emsp;30 minutes by Train</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="feutures">
+            <h4>FEATURES</h4>
+            <h5>GENERAL AMENITIES</h5>
+            <div class="row" id="line-content">
+                <div class="col-lg-4" id="hover-grey">
+                    <span>Cable TV</span>
+                </div>
+                <div class="col-lg-4" id="hover-grey">
+                    <span>Electric hot water</span>
+                </div>
+            </div>
+            <h5>ENERGY SAVINGS AMENITIES</h5>
+            <div class="row" id="line-content">
+                <div class="col-lg-4" id="hover-grey">
+                    <span>Gas Hot Water</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="map">
+            <h4>LOCATION</h4>
+        </div>
+
+        <div class="social">
+            <span><i class="fa fa-share-alt" aria-hidden="true"></i>&nbsp;Share</span>&emsp;
+            <span><i class="fa fa-facebook" aria-hidden="true"></i>&nbsp;Facebook</span>&emsp;
+            <span><i class="fa fa-twitter" aria-hidden="true"></i>&nbsp;Tweet</span>
+        </div>
+    </div>
+</div>
